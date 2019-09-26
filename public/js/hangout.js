2a84d2835f3ba12fac5609d9ad5c140c9660dd3d
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-26 15:31:39 -0400

    took out callback in api url

--- public/js/hangout.js
+++ public/js/hangout.js
@@ -9,10 +9,10 @@ let map;
 function initMap() {
     console.log("working");
     // let location = venue0;
-    // map = new google.maps.Map(document.getElementById('map'), {
-    //   center: {lat: -28.5383, lng: 81.3792},
-    //   zoom: 15
-    // });
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -28.5383, lng: 81.3792},
+      zoom: 15
+    });
     // searchResults.forEach(function(e, i) {
     //   makeMarker(locationResult[i]);
     // })
