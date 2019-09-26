17417cefca700aefd65a83a3cee31270bdd82a86
Dylan Davis <dylandavispc@gmail.com>
2019-09-26 14:48:25 -0400

    Login & Register successfully POST;  Directory to create-hangout page with MOCK authentication added;.

---
+++ public/js/register.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+    $(document).on("submit", "#reg", function() {
+      event.preventDefault();
+
+      const postUser = (userdata) =>{
+        $.post("/api/user", userdata)
+        .then(getUser)
+    };
+
+
+    const getUser = () => {
+      $.get("/api/user", function(data) {
+          for (let i = 0; i < data.length; i++) {
+    
+            if (email === data[i].email && password === data[i].password) {
+    
+              userId = data[i].id;
+    
+              login(userId);
+    
+            };
+          };
+        });
+    };
+
+      // Grabs email & password from login page
+      let name = $("#name").val().trim();
+      let email = $("#email").val().trim();
+      let password = $("#password").val().trim();
+
+      // POST Method
+      postUser({
+        name,
+        email,
+        password
+      });
+    });
+  });
+  
+  
+  const login = (userId) => {
+    location.href = "/create-hangout?user_id=" + userId;
+  }
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 // =============================================================
 require("./routes/html-routes.js")(app);
 // require("./routes/buds-api-routes.js")(app);
-// require("./routes/user-api-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
