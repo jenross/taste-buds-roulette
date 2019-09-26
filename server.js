cbe8627035c0a4a4f1f09cba3fd5fceb107de88a
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-26 11:36:05 -0400

    changed static to public on server file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("views"));
+app.use(express.static("public"));
 
 // Routes
 // =============================================================
