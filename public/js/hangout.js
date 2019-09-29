d2324cec790f85bbbbfc59b2ab52e3478ae006e6
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-29 13:35:15 -0400

    added in map and restaurant randomization logic

--- public/js/hangout.js
+++ public/js/hangout.js
@@ -1,78 +1,83 @@
-let locationInput = document.getElementById('enterLocation');
-let googleLogo = document.getElementById('google-logo');
+let resultsArr = [];
+let randomPick;
 
-let locationResult;
-let searchResults;
-var map; 
+const randomize = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-// Google Maps Initializer
 function initMap() {
-    console.log("working");
-    // let location = venue0;
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-          zoom: 15
-      });
-    // searchResults.forEach(function(e, i) {
-    //   makeMarker(locationResult[i]);
-    // })
-}
-
-// initMap();
-
-// gMaps Marker Creation
-function makeMarker(locationResult) {
-    var marker = new google.maps.Marker({position: searchResults.coords, map: map});
-    let infoWindow = new google.maps.InfoWindow({
-      content:searchResults.content
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13
     });
-    marker.addListener('click', function() {
-      infoWindow.open(map, marker);
-    })
+
+var input = document.getElementById('enterLocation');
+var autocomplete = new google.maps.places.Autocomplete(input);
+autocomplete.bindTo('bounds', map);
+autocomplete.setFields(
+    ['address_components', 'geometry', 'icon', 'name']);
+
+var infowindow = new google.maps.InfoWindow();
+var infowindowContent = document.getElementById('infowindow-content');
+infowindow.setContent(infowindowContent);
+
+var marker = new google.maps.Marker({
+  map: map,
+  anchorPoint: new google.maps.Point(0, -29)
+});
+
+autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+    $(".create-hangout").on("click", function(event) {
+        event.preventDefault();
+        console.log("working");
+        const APIKEY = "AIzaSyDWLRgKxz3nTinzcUXCyjM1DNpe9e4_g2w";
+        let lat = place.geometry.location.lat();
+        console.log(lat);
+        let lng = place.geometry.location.lng();
+        console.log(lng);
+        let initialQueryURL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${APIKEY}&location=${lat},${lng}&radius=1000&type=restaurant`;
+        $.ajax({
+            url: initialQueryURL,
+            method: "GET"
+        }).then(function(response) {
+            for (let i = 0; i < response.results.length; i++) {
+                let place_id = response.results[i].place_id;
+                resultsArr.push(place_id);
+                randomPick = resultsArr[randomize(0, resultsArr.length - 1)];
+            }
+            console.log(randomPick);
+            let pickedQueryURL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?key=${APIKEY}&place_id=${randomPick}`;
+            $.ajax({
+                url: pickedQueryURL,
+                method: "GET"
+            }).then(function(response) {
+                let pickName = response.result.name;
+                console.log(pickName);
+                map.setCenter(response.result.geometry.location);
+                map.setZoom(17);  // Why 17? Because it looks good.
+                // }
+                marker.setPosition(response.result.geometry.location);
+                marker.setVisible(true);
+
+                var address = response.result.formatted_address;
+                
+
+                infowindowContent.children['place-icon'].src = response.result.icon;
+                infowindowContent.children['place-name'].textContent = response.result.name;
+                infowindowContent.children['place-address'].textContent = address;
+                infowindow.open(map, marker);
+                });
+            });
+        });
+    });
 }
 
-// let map;
-// let service;
-// let infowindow;
 
-// function initMap() {
-//     let sydney = new google.maps.LatLng(-33.867, 151.195);
-
-//     infowindow = new google.maps.InfoWindow();
-
-//     map = new google.maps.Map(
-//         document.getElementById('map'), {center: sydney, zoom: 15});
-
-//     let request = {
-//         query: 'Museum of Contemporary Art Australia',
-//         fields: ['name', 'geometry'],
-//     };
-
-//     service = new google.maps.places.PlacesService(map);
-
-//     service.findPlaceFromQuery(request, function(results, status) {
-//         if (status === google.maps.places.PlacesServiceStatus.OK) {
-//         for (var i = 0; i < results.length; i++) {
-//             createMarker(results[i]);
-//         }
-//         map.setCenter(results[0].geometry.location);
-//     }
-//     });
-// }
-
-// function createMarker(place) {
-//     var marker = new google.maps.Marker({
-//     map: map,
-//     position: place.geometry.location
-//     });
-
-//     google.maps.event.addListener(marker, 'click', function() {
-//     infowindow.setContent(place.name);
-//     infowindow.open(map, this);
-//     });
-// }
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // Added Code from Dylan
 
 $(document).ready(function() {
